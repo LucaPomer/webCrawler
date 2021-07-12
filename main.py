775cc7a9d98f6707216d74933f0ea4d63cd647ad
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-07-12 18:32:38 +0200

    dowland a page and get a tag

--- main.py
+++ main.py
@@ -5,19 +5,29 @@
 import requests
 from bs4 import BeautifulSoup
 
+## {"src": "https://cdn.weddyplace.com/static/misc/badges/badge-gelistet.png"}
 
 def get_website_content(url):
     page = requests.get(url)
     print(page.status_code)
     # print(page.content)
     soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
     # print(soup.prettify())
-    html = list(soup.children)[2]
-    print(html)
-
-    all_p_tags = soup.find_all('p')
-    print(all_p_tags)
-    print(all_p_tags[0].get_text())
 
 
-get_website_content("https://dataquestio.github.io/web-scraping-pages/simple.html")
+def find_tag_in_content(soup, tag):
+    # print(soup.children)
+    # print(soup.prettify())
+    # all_p_tags = soup.find_all(tag)
+    # print(all_p_tags)
+    # print(all_p_tags[0].get_text())
+    weddy_badge = soup.findAll("link")
+    print(weddy_badge)
+
+
+page_soup: BeautifulSoup = get_website_content(
+    "https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/")
+
+find_tag_in_content(page_soup, 'p')
+

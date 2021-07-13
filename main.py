7880b0993e14c41891afd31855ecb953ae65b06a
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-07-13 16:41:59 +0200

    finding link to vendors page + going over all http links

--- main.py
+++ main.py
@@ -7,65 +7,47 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-
+# main_site =  "https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/"
+main_site = "http://www.andreaslemke.com/"
 ## {"src": "https://cdn.weddyplace.com/static/misc/badges/badge-gelistet.png"}
 
-def get_website_content(url):
-    page = requests.get(url)
-    print(page.status_code)
-    # print(page.content)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    all_sub_sites = soup.findAll('a', href=True)
-    # print(all_sub_sites)
-    return soup
-    # print(soup.prettify())
 
-
-def find_tag_in_content(soup, tag):
+def find_tag_in_content(soup):
     # print(soup.children)
     # print(soup.prettify())
-    # all_p_tags = soup.find_all(tag)
-    # print(all_p_tags)
-    # print(all_p_tags[0].get_text())
-    weddy_badge = soup.findAll("link")
-    # print(weddy_badge)
+    all_p_tags = soup.find_all('a', href=re.compile('^https://www.weddyplace.com/vendors/'))
+    for element in all_p_tags:
+        print(element)
+        ## todo: check if the source is the same
 
 
-def get_links(page_url, pages):
-    # pages = set()
-    # main_url = page_url.split('/')
-    # main_page = "https://" + main_url[2]
-    # print(main_page)
-    pattern = re.compile("^(/)")
+
+def page_is_sub_page(original_page, new_url):
+    splitted_og_page = original_page.split('/')
+    domain = splitted_og_page[2]
+    if domain in new_url: ## todo: add checking if the new url is just a /something
+        return True
+    else:
+        return False
+
+def get_links(page_url, pages, start_page):
     html = requests.get(page_url)  # fstrings require Python 3.6+
     soup = BeautifulSoup(html.content, 'html.parser')
     # print(soup.prettify())
     # print(soup.findAll('a', href=True))
     for link in soup.find_all("a", href=True):
         new_link = link['href']
-        print(new_link)
-        if new_link not in pages and 'http' in new_link:
+        if new_link not in pages and page_is_sub_page(start_page, new_link):
+            print(new_link)
             pages.add(new_link)
-            get_links(new_link,pages)
+            find_tag_in_content(soup)
+            get_links(new_link,pages, start_page)
 
-    print(pages)
-        # href = link.attrs["href"]
-        # print("link " + str(href))
-        # if "href" in link.attrs:
-        #     if main_page in href:
-        #         new_page = href
-        #         print("FOUND")
-        #     else:
-        #         new_page = main_page + link.attrs["href"]
-        #     if new_page not in pages:
-        #         print('new page ' + str(new_page))
-        #         pages.add(new_page)
-        #         print(pages)
-        #         get_links(new_page, pages)
+    return pages
 
 
-page_soup: BeautifulSoup = get_website_content(
-    "https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/")
 
-find_tag_in_content(page_soup, 'p')
-get_links("https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/", set())
+# find_tag_in_content(page_soup, 'p')
+all_sub_links = get_links(main_site, set(), main_site)
+# print(all_sub_links)
+

4e1e7ba6377bab91dfc55b4d559e34e00f1391f8
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-07-20 11:19:00 +0200

    improved error handling

--- crawler.py
+++ crawler.py
@@ -1,13 +1,20 @@
+import functools
 import re
 import requests
 from bs4 import BeautifulSoup
 from helpers.data_writer import write_multiple_lines, write_data
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type, wait_exponential
+
+
+class NoBadgeFoundError(Exception):
+    """Raised when there is no badge found"""
+    pass
 
 
 def find_badge_in_content(soup):
     badge_tag = soup.find('a', href=re.compile('^https://www.weddyplace.com/vendors/'))
     if badge_tag is None:
-        return None
+        raise NoBadgeFoundError
     badge_element = []
     img = badge_tag.find('img')
     image_src = img['data-src']
@@ -16,7 +23,7 @@ def find_badge_in_content(soup):
     return badge_element
 
 
-def page_is_sub_page(original_page, new_url):
+def page_is_sub_page(original_page, new_url):  # todo: find a library that checks if is a sub library + add docs
     splitted_og_page = original_page.split('/')
     domain = splitted_og_page[2]
     if domain in new_url:
@@ -25,21 +32,40 @@ def page_is_sub_page(original_page, new_url):
         return False
 
 
-def get_all_sub_links(page_url, start_page, all_urls, soups_to_url):
+@retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=4, max=60),
+       retry=retry_if_exception_type(requests.ConnectionError))
+def get_all_sub_links(page_url, start_page=None, traversed_urls=None, soups_to_url=None):
+    # initializing variables for head of recursive function
+    global is_sub_page
+    if soups_to_url is None:
+        soups_to_url = dict()
+    if start_page is None:
+        start_page = page_url
+        is_sub_page = functools.partial(page_is_sub_page, page_url)
+    if traversed_urls is None:
+        traversed_urls = set()
+
     try:
         html = requests.get(page_url, timeout=10)
         soup = BeautifulSoup(html.content, 'html.parser')
-        soups_to_url.append([soup, page_url])
+        soups_to_url[soup] = page_url
         for link in soup.find_all("a", href=True):
             new_link = link['href']
-            if new_link not in all_urls and page_is_sub_page(start_page, new_link):
-                # print(new_link)
-                all_urls.add(new_link)
-                get_all_sub_links(new_link, start_page, all_urls, soups_to_url)
+            # link was already traversed
+            if new_link in traversed_urls:
+                continue
+            # link is not part of domain
+            if not is_sub_page(new_link):
+                continue
+
+            traversed_urls.add(new_link)
+            get_all_sub_links(new_link, start_page, traversed_urls, soups_to_url)
+
     except requests.ConnectionError as e:
         print(
             "OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.\n")
         print(str(e))
+        raise
     except requests.Timeout as e:
         print("OOPS!! Timeout Error")
         print(str(e))
@@ -47,9 +73,8 @@ def get_all_sub_links(page_url, start_page, all_urls, soups_to_url):
         print('Wrong URL scema: URL = ' + str(page_url))
     except requests.RequestException as e:
         print("OOPS!! General Error")
+        print(page_url + " was not successful")
         print(str(e))
-    except KeyboardInterrupt:
-        print("Someone closed the program")
 
     return soups_to_url
 
@@ -65,5 +90,3 @@ def search_and_write_badges(page_soupes_to_url, csv_file_path):
             found_badge.append(url)
             write_data(found_badge, csv_file_path)
     write_data(['Found Badges ', str(num_badges)], csv_file_path)
-
-
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import requests
+
 from crawler import get_all_sub_links, search_and_write_badges
 
-site_to_crawl = "https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/"
-# site_to_crawl = "http://www.andreaslemke.com/"
-result_csv = 'result_files/result.csv'
 
-sub_pages = {site_to_crawl}
-soups_to_url = []
+def main():
+    site_to_crawl = "https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/"
+    # site_to_crawl = "http://www.andreaslemke.com/"
+    result_csv = 'result_files/result.csv'
+    try:
+        soup_to_url_for_all_sub_pages = get_all_sub_links(site_to_crawl)
+        search_and_write_badges(soup_to_url_for_all_sub_pages, result_csv)
+    except requests.ConnectionError as e:
+        print("crawling not possible due to a connection error, please try again later")
+        print(e)
 
-soup_to_url_for_all_sub_pages = get_all_sub_links(site_to_crawl, site_to_crawl, sub_pages, soups_to_url)
-search_and_write_badges(soup_to_url_for_all_sub_pages, result_csv)
+
+if __name__ == "__main__":
+    main()

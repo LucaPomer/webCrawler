6e0e330214b9376e2f539623978128be0775c8e1
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-07-20 12:21:02 +0200

    improved url selection

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import functools
 import re
+from urllib.parse import urlparse
+
 import requests
 from bs4 import BeautifulSoup
 from helpers.data_writer import write_multiple_lines, write_data
@@ -23,13 +25,27 @@ def find_badge_in_content(soup):
     return badge_element
 
 
-def page_is_sub_page(original_page, new_url):  # todo: find a library that checks if is a sub library + add docs
-    splitted_og_page = original_page.split('/')
-    domain = splitted_og_page[2]
-    if domain in new_url:
-        return True
-    else:
+# checks weather the new link is part of the original domain
+def page_is_part_of_domain(original_page, new_url):
+    domain = urlparse(original_page).netloc
+    return domain in new_url
+
+
+def desired_links(href):
+    parsed = urlparse(href)
+    # ignore if href is not set
+    if not href:
         return False
+
+    # ignore if it is just a link to the same page
+    if href.startswith("#"):
+        return False
+
+    # ignore if it does not contain a proper protocol (http/https)
+    if not bool(parsed.scheme):
+        return False
+
+    return True
 
 
 @retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=4, max=60),
@@ -41,7 +57,7 @@ def get_all_sub_links(page_url, start_page=None, traversed_urls=None, soups_to_u
         soups_to_url = dict()
     if start_page is None:
         start_page = page_url
-        is_sub_page = functools.partial(page_is_sub_page, page_url)
+        is_sub_page = functools.partial(page_is_part_of_domain, page_url)
     if traversed_urls is None:
         traversed_urls = set()
 
@@ -49,15 +65,11 @@ def get_all_sub_links(page_url, start_page=None, traversed_urls=None, soups_to_u
         html = requests.get(page_url, timeout=10)
         soup = BeautifulSoup(html.content, 'html.parser')
         soups_to_url[soup] = page_url
-        for link in soup.find_all("a", href=True):
+        for link in soup.find_all("a", href=desired_links):
             new_link = link['href']
-            # link was already traversed
-            if new_link in traversed_urls:
+            # link was already traversed or is not part of domain
+            if new_link in traversed_urls or not is_sub_page(new_link):
                 continue
-            # link is not part of domain
-            if not is_sub_page(new_link):
-                continue
-
             traversed_urls.add(new_link)
             get_all_sub_links(new_link, start_page, traversed_urls, soups_to_url)
 
@@ -80,13 +92,18 @@ def get_all_sub_links(page_url, start_page=None, traversed_urls=None, soups_to_u
 
 
 def search_and_write_badges(page_soupes_to_url, csv_file_path):
-    num_badges = 0
-    for elem in page_soupes_to_url:
-        soup = elem[0]
-        url = elem[1]
-        found_badge = find_badge_in_content(soup)
-        if found_badge:
-            num_badges += 1
+    all_badges = get_badges(page_soupes_to_url)
+    write_multiple_lines(all_badges, csv_file_path)
+    write_data(['Found Badges ', str(len(all_badges))], csv_file_path)
+
+
+def get_badges(page_soupes_to_url):
+    all_badges = []
+    for soup, url in page_soupes_to_url.items():
+        try:
+            found_badge = find_badge_in_content(soup)
             found_badge.append(url)
-            write_data(found_badge, csv_file_path)
-    write_data(['Found Badges ', str(num_badges)], csv_file_path)
+            all_badges.append(found_badge)
+        except NoBadgeFoundError as e:
+            continue
+    return all_badges

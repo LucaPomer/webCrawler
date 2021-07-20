76c2319360fa736bfe101a87d3442e97116bdf50
Luca Pomer <43848253+LucaPomer@users.noreply.github.com>
2021-07-20 20:22:00 +0200

    added documentation to functions

--- crawler.py
+++ crawler.py
@@ -1,7 +1,6 @@
 import functools
 import re
 from urllib.parse import urlparse
-
 import requests
 from bs4 import BeautifulSoup
 from helpers.data_writer import write_multiple_lines, write_data
@@ -13,24 +12,35 @@ class NoBadgeFoundError(Exception):
     pass
 
 
-def find_badge_in_content(soup):
-    badge_tag = soup.find('a', href=re.compile('^https://www.weddyplace.com/vendors/'))
+###
+# checks weather the content contains the badge tag
+# input: soup - BeautifulSoup, href_regex - regex
+# output: [badge_tag - string, image_src - string]
+###
+def find_badge_in_content(soup, href_regex):
+    badge_tag = soup.find('a', href=href_regex)
     if badge_tag is None:
-        raise NoBadgeFoundError
-    badge_element = []
+        raise NoBadgeFoundError()
     img = badge_tag.find('img')
     image_src = img['data-src']
-    badge_element.append(badge_tag['href'])
-    badge_element.append(image_src)
-    return badge_element
+    return [badge_tag['href'], image_src]
 
 
+###
 # checks weather the new link is part of the original domain
+# input: original_page - string, new_url - string
+# output: boolean
+###
 def page_is_part_of_domain(original_page, new_url):
     domain = urlparse(original_page).netloc
     return domain in new_url
 
 
+###
+# defines which href elements in the HTML content are desired
+# input: href-string
+# output: boolean
+###
 def desired_links(href):
     parsed = urlparse(href)
     # ignore if href is not set
@@ -48,30 +58,17 @@ def desired_links(href):
     return True
 
 
+###
+# sends a request to get the html
+# input: url-string
+# output: html.content
+###
 @retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=4, max=60),
        retry=retry_if_exception_type(requests.ConnectionError))
-def get_all_sub_links(page_url, start_page=None, traversed_urls=None, soups_to_url=None):
-    # initializing variables for head of recursive function
-    global is_sub_page
-    if soups_to_url is None:
-        soups_to_url = dict()
-    if start_page is None:
-        start_page = page_url
-        is_sub_page = functools.partial(page_is_part_of_domain, page_url)
-    if traversed_urls is None:
-        traversed_urls = set()
-
+def get_html_content(url):
     try:
-        html = requests.get(page_url, timeout=10)
-        soup = BeautifulSoup(html.content, 'html.parser')
-        soups_to_url[soup] = page_url
-        for link in soup.find_all("a", href=desired_links):
-            new_link = link['href']
-            # link was already traversed or is not part of domain
-            if new_link in traversed_urls or not is_sub_page(new_link):
-                continue
-            traversed_urls.add(new_link)
-            get_all_sub_links(new_link, start_page, traversed_urls, soups_to_url)
+        html = requests.get(url, timeout=10)
+        return html.content
 
     except requests.ConnectionError as e:
         print(
@@ -81,29 +78,75 @@ def get_all_sub_links(page_url, start_page=None, traversed_urls=None, soups_to_u
     except requests.Timeout as e:
         print("OOPS!! Timeout Error")
         print(str(e))
+        raise
     except requests.exceptions.MissingSchema as e:
-        print('Wrong URL scema: URL = ' + str(page_url))
+        print(f'Wrong URL schema: {url}')
+        raise
     except requests.RequestException as e:
         print("OOPS!! General Error")
-        print(page_url + " was not successful")
+        print(f"{url} content was not retrieved")
         print(str(e))
+        raise
+
+
+###
+# gets all sub links and their soup for a url
+# input: url-string
+# output: soup to string dictionary
+###
+def get_all_sub_links(page_url, is_sub_page=None, traversed_urls=None, soups_to_url=None):
+    # initializing variables for head of recursive function
+    if soups_to_url is None:
+        soups_to_url = dict()
+    if is_sub_page is None:
+        is_sub_page = functools.partial(page_is_part_of_domain, page_url)
+    if traversed_urls is None:
+        traversed_urls = set()
+
+    try:
+        html_content = get_html_content(page_url)
+
+    except requests.exceptions as e:
+        print("there was an error, crawling stopped")
+        print(str(e))
+        return soups_to_url
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    soups_to_url[soup] = page_url
+    for link in soup.find_all("a", href=desired_links):
+        new_link = link['href']
+        # link was already traversed or is not part of domain
+        if new_link in traversed_urls or not is_sub_page(new_link):
+            continue
+        traversed_urls.add(new_link)
+        get_all_sub_links(new_link, is_sub_page, traversed_urls, soups_to_url)
 
     return soups_to_url
 
 
-def search_and_write_badges(page_soupes_to_url, csv_file_path):
-    all_badges = get_badges(page_soupes_to_url)
+###
+# delegates the searching of the badges and prints the result
+# input: page_soups_to_url-dictionary, badge_tag_regex-regex, csv_file_path-string
+# output: none
+###
+def search_and_write_badges(page_soups_to_url, badge_tag_regex, csv_file_path):
+    all_badges = get_badges(page_soups_to_url, badge_tag_regex)
     write_multiple_lines(all_badges, csv_file_path)
     write_data(['Found Badges ', str(len(all_badges))], csv_file_path)
 
 
-def get_badges(page_soupes_to_url):
+###
+# goes over all pages and searches for the badge
+# input: page_soups_to_url-dictionary, badge_tag_regex-regex
+# output: an array of string arrays
+###
+def get_badges(page_soups_to_url, badge_tag_regex):
     all_badges = []
-    for soup, url in page_soupes_to_url.items():
+    for soup, url in page_soups_to_url.items():
         try:
-            found_badge = find_badge_in_content(soup)
+            found_badge = find_badge_in_content(soup, badge_tag_regex)
             found_badge.append(url)
             all_badges.append(found_badge)
-        except NoBadgeFoundError as e:
+        except NoBadgeFoundError:
             continue
     return all_badges
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 
 from crawler import get_all_sub_links, search_and_write_badges
@@ -5,11 +7,11 @@ from crawler import get_all_sub_links, search_and_write_badges
 
 def main():
     site_to_crawl = "https://www.spree-liebe.de/hochzeitsfotograf-berlin-empfehlung/weddyplace/"
-    # site_to_crawl = "http://www.andreaslemke.com/"
     result_csv = 'result_files/result.csv'
     try:
         soup_to_url_for_all_sub_pages = get_all_sub_links(site_to_crawl)
-        search_and_write_badges(soup_to_url_for_all_sub_pages, result_csv)
+        search_and_write_badges(soup_to_url_for_all_sub_pages, re.compile('^https://www.weddyplace.com/vendors/'),
+                                result_csv)
     except requests.ConnectionError as e:
         print("crawling not possible due to a connection error, please try again later")
         print(e)
